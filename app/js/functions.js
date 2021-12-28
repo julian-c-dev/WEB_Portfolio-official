8eec54c72e61eb64583e7fa3f23a2e0376dd45a6
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-12-28 16:56:35 +0000

    Updated - mobile to desktop.

--- app/js/functions.js
+++ app/js/functions.js
@@ -2,6 +2,7 @@
 const desktopNav = document.querySelector(".nav__desktop");
 const mobileNav = document.querySelector(".nav__mobile");
 const section = document.querySelector(".section");
+const title = document.querySelector(".title");
 
 const stwch_Theme = document.querySelector("#handlerTheme");
 
@@ -11,4 +12,5 @@ function themeEvent() {
   mobileNav.classList.toggle("dark");
   desktopNav.classList.toggle("dark");
   section.classList.toggle("dark");
+  title.classList.toggle("darkest");
 }
--- app/js/main.js
+++ app/js/main.js
@@ -35,40 +35,6 @@ function flipping(element) {
 let menuOpen = false;
 let flagClicked = false;
 
-// * enable hidden navBar
-
-const nav = document.querySelector(".navBar");
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-  if (
-    menuOpen === false &&
-    window.scrollY > 200 &&
-    lastScrollY < window.scrollY
-  ) {
-    nav.classList.add("navBar--hidden");
-    closeOpenItems();
-  }
-  if (lastScrollY > window.scrollY) {
-    nav.classList.remove("navBar--hidden");
-  }
-
-  lastScrollY = window.scrollY;
-});
-
-// * #menu hidden when resizing more than 700px
-
-window.addEventListener("resize", menuOffAfter700);
-
-function menuOffAfter700() {
-  if (window.screen.width >= 700) {
-    menuDiv.style.display = "none";
-    menuOpen = false;
-    menuBtn.classList.remove("open");
-    menuBoolean = true;
-  }
-}
-
 // * menu-btn animation = menu icon to X
 const burgerFrame = document.querySelector(".burger__frame");
 
