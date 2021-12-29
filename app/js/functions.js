00ed0fb63a7359f4de86cb87219b8b75f9a4d832
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-12-29 10:58:07 +0000

    Builted section About

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,7 +1,7 @@
 // & TOGGLE Theme dark/light
 const desktopNav = document.querySelector(".nav__desktop");
 const mobileNav = document.querySelector(".nav__mobile");
-const section = document.querySelector(".section");
+const section = document.querySelectorAll(".section");
 const title = document.querySelector(".title");
 
 const stwch_Theme = document.querySelector("#handlerTheme");
@@ -11,6 +11,8 @@ stwch_Theme.addEventListener("click", themeEvent);
 function themeEvent() {
   mobileNav.classList.toggle("dark");
   desktopNav.classList.toggle("dark");
-  section.classList.toggle("dark");
   title.classList.toggle("darkest");
+  for (let i = 0; i < section.length; i++) {
+    section[i].classList.toggle("dark");
+  }
 }
