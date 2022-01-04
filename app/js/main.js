fc1a78acab9b508bf7f96efe635dee8821abe710
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-04 21:20:27 +0000

    Updated breakpoint 636px to 680px

--- app/js/main.js
+++ app/js/main.js
@@ -48,6 +48,6 @@ burgerFrame.addEventListener("click", () => {
   }
 });
 
-// * .frame-logos from flex-clm to grid at 636px
+// * .frame-logos from flex-clm to grid at 680px
 
 const containerLogos = document.querySelector(".frame-logos ");
