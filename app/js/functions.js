1a65bd26513e007ab1b1b176d21242f215a8491e
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-07 19:29:25 +0000

    Updating Slider

--- app/js/functions.js
+++ app/js/functions.js
@@ -17,9 +17,70 @@ function themeEvent() {
   }
 }
 
-// & REMOVE CLASSES after 1200px
-const swiper1 = document.querySelector(".job-swiper-frame");
-const swiper2 = document.querySelector(".swiper");
-const swiper3 = document.querySelectorAll(".mySwiper");
-const swiper4 = document.querySelector(".swiper-wrapper");
-const swiper5 = document.querySelector(".swiper-slide");
+// & REMOVE CLASSES after 1200px (Exp section, normal => slider cards)
+const swiper1 = document.querySelector(".swiper1");
+const swiper2 = document.querySelector(".swiper2");
+const swiper3 = document.querySelector(".swiper3");
+const swiper4 = document.querySelector(".swiper4");
+let swiper = null;
+
+window.onload = function () {
+  if (window.screen.width >= 1200 && window.innerWidth >= 1200) {
+    swiper1.classList.remove("job-swiper-frame");
+    swiper2.classList.remove("swiper");
+    swiper2.classList.remove("mySwiper");
+    swiper3.classList.remove("swiper-wrapper");
+    swiper4.classList.remove("swiper-slide");
+    swiper = null;
+  } else {
+    swiper1.classList.add("job-swiper-frame");
+    swiper2.classList.add("swiper");
+    swiper2.classList.add("mySwiper");
+    swiper3.classList.add("swiper-wrapper");
+    swiper4.classList.add("swiper-slide");
+    swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 100,
+        depth: 800,
+        modifier: 1,
+        slideShadows: false,
+      },
+    });
+  }
+};
+
+window.addEventListener("resize", onResizeFunction);
+function onResizeFunction() {
+  if (window.screen.width >= 1200 && window.innerWidth >= 1200) {
+    swiper1.classList.remove("job-swiper-frame");
+    swiper2.classList.remove("swiper");
+    swiper2.classList.remove("mySwiper");
+    swiper3.classList.remove("swiper-wrapper");
+    swiper4.classList.remove("swiper-slide");
+    swiper = null;
+  } else {
+    swiper1.classList.add("job-swiper-frame");
+    swiper2.classList.add("swiper");
+    swiper2.classList.add("mySwiper");
+    swiper3.classList.add("swiper-wrapper");
+    swiper4.classList.add("swiper-slide");
+    swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 100,
+        depth: 800,
+        modifier: 1,
+        slideShadows: false,
+      },
+    });
+  }
+}
