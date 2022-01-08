d958f532f642b50c48527220dae039efb985cde0
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-08 14:24:51 +0000

    Updated Skills cards responsive

--- app/js/functions.js
+++ app/js/functions.js
@@ -83,3 +83,37 @@ function onResizeFunction() {
     });
   }
 }
+
+//* Slide for Skills Section
+
+let slideIndex = 3;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+window.onload = function () {
+  if (window.screen.width >= 700 && window.innerWidth >= 700) {
+  }
+};
