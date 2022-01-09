2eca7a61a907feccc25441d291c6319dc8283f58
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-09 18:38:18 +0000

    Updated functions.js

--- app/js/functions.js
+++ app/js/functions.js
@@ -16,6 +16,40 @@ function themeEvent() {
   }
 }
 
+// & [Skill Section] Slider
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("slide-circle");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" hlgt", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " hlgt";
+}
+
 // & REMOVE CLASSES after 1250px (Exp section, normal => slider cards)
 const swiper1 = document.querySelector(".swiper1");
 const swiper2 = document.querySelector(".swiper2");
@@ -83,42 +117,3 @@ function onResizeFunction() {
     });
   }
 }
-
-//* Slide for Skills Section
-
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
-
-function currentDiv(n) {
-  showDivs((slideIndex = n));
-}
-
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("slide-circle");
-
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" hlgt", "");
-  }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " hlgt";
-}
-
-window.onload = function () {
-  if (window.screen.width >= 700 && window.innerWidth >= 700) {
-  }
-};
