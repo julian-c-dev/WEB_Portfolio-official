4b5e6852d1f1296b1dd8e58c976b210fe2f6c3d3
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-13 20:50:19 +0000

    Doing light mode

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,8 +1,5 @@
 // & [NavBar] TOGGLE btwn Theme dark/light
-const desktopNav = document.querySelector(".nav__desktop");
-const mobileNav = document.querySelector(".nav__mobile");
-const section = document.querySelectorAll(".section");
-const title = document.querySelector(".title");
+
 const stwch_Theme = document.querySelector("#handlerTheme");
 const stwch_ThemeHidden = document.querySelector("#handlerThemeHidden");
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -30,14 +27,7 @@ function themeEventMobile() {
 
 checkboxes.forEach((element) => element.addEventListener("click", toggleTheme));
 
-function toggleTheme() {
-  mobileNav.classList.toggle("dark");
-  desktopNav.classList.toggle("dark");
-  title.classList.toggle("darkest");
-  for (let i = 0; i < section.length; i++) {
-    section[i].classList.toggle("dark");
-  }
-}
+function toggleTheme() {}
 
 // & [Skill Section] Slider
 
